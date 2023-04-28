93ca8919b445b3d30498329a5585090b52a35628
achyuthask <96536973+achyuthask@users.noreply.github.com>
2023-04-28 23:21:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const pic=  React.createElement('img', {src:'https://cdn.wallpapersafari.com/29/32/zdpXbg.jpg' ,alt:" "}, );
+  const  para1=React.createElement('h1' , {}, 'Naruto uzumaki');
+  const  para2=React.createElement('p' , {}, 'Lorem ipsum dolor sit amet');
+  const  para3=React.createElement('p','br' , [], '100',<br></br>,'posts');
+  const  para4=React.createElement('p' ,'br' ,[], '50',<br></br>,'following');
+  const  para5=React.createElement('p' ,'br' ,[], '1.0B',<br></br>,'followers');
+  const  para6=React.createElement('button' , {}, 'Follow');
+  const  para7=React.createElement('button' , {}, 'message');
+
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+     {pic}
+     {para1}
+     {para2}
+     <div className='card-part'>
+     {para3}
+     {para4}
+     {para5}
+
+     </div>
+     <div className='card-part2'>
+     {para6}
+     {para7}
+
+     </div>
+
     </div>
   );
 }
